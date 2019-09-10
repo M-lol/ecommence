e805f0fe2d5f732d85f10f7632f8cd49ae8c7e31
Mohammed Refat <some@email.com>
2019-09-10 09:23:09 +0200

    Initial commit

---
+++ src/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp  = require('gulp');
+
+gulp.task('watch',function() {
+    console.log("Hello from Gulp");
+});
+
+
+gulp.task('html',function(){
+    return gulp.src('src/*.html')
+    .pipe(gulp.dest('build'));
+});
